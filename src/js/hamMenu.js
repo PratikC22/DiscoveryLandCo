988812d35f9de3cbfa1231d64610c1f5746ddab1
Pratik Chaudhari <pratikc1020@gmail.com>
2021-10-13 20:17:20 +0530

    added animation

--- src/js/hamMenu.js
+++ src/js/hamMenu.js
@@ -5,7 +5,7 @@ dropDownIcon.addEventListener('onclick', dropDownMenu());
 
 function dropDownMenu() {
     console.log(hide);
-    const html = `<div class="main-container">
+    const html = `<div class="main-container" data-aos="fade-down" data-aos-duration="2000">
         <div class="nav-container">
             <div class="hamlogo">
                 <img class="hamlogo" id="hlogo" src="../assets/logo1.svg" alt="">
@@ -21,6 +21,13 @@ function dropDownMenu() {
             <a class="menulinks" id="l4" href="#">Careers</a>
             <a class="menulinks" id="l5" href="#">Contact</a>
         </div>
+        <div class="hamLinks">
+                <div class="hamitem"><a class="navlink-anchor" href="./aboutDLC.html"> About DLC </a></div>
+                <div class="hamitem"><a class="navlink-anchor" href="./ourWorlds.html"> Our Worlds </a> </div>
+                <div class="hamitem"><a class="navlink-anchor" href="./experiences.html"> Experiences </a> </div>
+                <div class="hamitem"><a class="navlink-anchor" href="./gallery.html"> Gallery </a> </div>
+                <div class="hamitem"><a class="navlink-anchor" href="./press.html"> Press </a></div>
+            </div>
     </div>`
     const div = document.getElementById("hambergMenu");
     div.innerHTML = html;
